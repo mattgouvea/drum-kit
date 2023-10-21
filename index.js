dd77cc2f746a8b3139e613b5558d43eb62a3e285
Matheus Abrantes <45212366+mattgouvea@users.noreply.github.com>
2023-10-20 22:15:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+var buttons = document.getElementsByClassName("drum")
+
+function playSound(key) {
+
+    switch (key) {
+        case "w":
+            var audio = new Audio("./sounds/tom-1.mp3")
+            audio.play()
+            break;
+        
+        case "a":
+            var audio = new Audio("./sounds/tom-2.mp3")
+            audio.play()
+            break;
+        
+        case "s":
+            var audio = new Audio("./sounds/tom-3.mp3")
+            audio.play()
+            break;
+        
+        case "d":
+            var audio = new Audio("./sounds/tom-4.mp3")
+            audio.play()
+            break;
+
+        case "j":
+            var audio = new Audio("./sounds/crash.mp3")
+            audio.play()
+            break;
+
+        case "k":
+            var audio = new Audio("./sounds/kick-bass.mp3")
+            audio.play()
+            break;
+
+        case "l":
+            var audio = new Audio("./sounds/snare.mp3")
+            audio.play()
+            break;
+
+        default:
+            break;
+        
+    }
+
+}
+
+for(i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() { playSound(this.innerHTML); animateButton(this.innerHTML); })
+}
+
+document.addEventListener("keypress", function(event) { playSound(event.key); animateButton(event.key); })
+
+function animateButton(key) {
+    var pressedKey = document.querySelector("." + key)
+    pressedKey.classList.add("pressed")
+    setTimeout(function() { pressedKey.classList.remove("pressed") }, 100)
+}
